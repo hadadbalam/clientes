091d7a45c55f3bc05b3bdfad0c97c0b9d380da1e
hadad <haddyb0@gmail.com>
2026-04-15 18:52:22 -0600

    Fix: agregar componentes y directivas a vuetify plugin

--- clientes-sistema/src/plugins/vuetify.js
+++ clientes-sistema/src/plugins/vuetify.js
@@ -1,19 +1,25 @@
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+// 1. IMPORTA ESTO (Es vital para el build de Vercel)
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 export default createVuetify({
+  // 2. REGISTRA LOS COMPONENTES
+  components,
+  directives,
   theme: {
     defaultTheme: 'miTema',
     themes: {
       miTema: {
         dark: false,
         colors: {
-          background: '#DBD2C9',   // fondo claro
-          surface: '#DBD2C9',      // tarjetas
-          primary: '#7F1D25',      // 🔥 rojo cereza (principal)
-          secondary: '#9F6162',    // tono medio
-          accent: '#CD9395',       // rosa suave
-          error: '#63131C',        // rojo oscuro fuerte
+          background: '#DBD2C9',
+          surface: '#DBD2C9',
+          primary: '#7F1D25',
+          secondary: '#9F6162',
+          accent: '#CD9395',
+          error: '#63131C',
           info: '#CD9395'
         }
       }
